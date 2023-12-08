5e29e5015b5f71fbb756b13234066cc9a09ace3e
Elaina Cole <elainacole01@gmail.com>
2023-12-08 03:14:09 -0500

    Add files via upload

---
+++ day5/Mapping.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode
+{
+	public class Mapping
+	{
+
+        public ulong MinSourceNumber { get; set; }
+        public ulong MaxSourceNumber { get; set; }
+
+        public ulong DifferenceToDestination { get; set; }
+
+        public ulong MinDestination { get; set; }
+        public ulong MaxDestination { get; set; }
+    }
+}
+
---
+++ day5/MappingType.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode
+{
+	public enum MappingType
+	{
+        None,
+        SeedToSoil,
+        SoilToFertilizer,
+        FertilizerToWater,
+        WaterToLight,
+        LightToTemperature,
+        TemperatureToHumidity,
+        HumidityToLocation
+    }
+}
+
